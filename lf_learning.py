9f8d9334b20a495d36c0d63673ef5487a3eb8411
hong <35002339+muchengxuev587@users.noreply.github.com>
2023-12-07 11:10:20 +0800

    Update lf_learning.py

--- lf_learning.py
+++ lf_learning.py
@@ -2,7 +2,7 @@ from motoko_project import Motoko
 
 
 # You can also use mc_port instead of azure_login, but azure_login is highly recommended
-openai_api_key = "sk-lAmlRoBhqOiU7Zz82aBeAc84969d4e3cB338326cD5A0855c"
+openai_api_key = ""
 openai_base = "https://api.ezchat.top/v1"
 motoko = Motoko(
     openai_api_key=openai_api_key,
